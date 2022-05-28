dbf37dd78dced8489eec0c4ab37f39d2cd701fd2
Aidan Medcalf <aidanmedcalf@gmail.com>
2022-05-27 21:24:32 -0500

    Cleaned up Neutron_CLI.py

--- Neutron_CLI.py
+++ Neutron_CLI.py
@@ -7,25 +7,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 import scipy
-#import PySimpleGUI as sg      
 import os
 
 from argparse import ArgumentParser
 from functools import partial
 from datetime import timedelta
 
-
 crc_poly = 0x104C11DB7  # 0x11EDC6F41
 crc_seed = 0x100000007 # 0xB319AFF0
-#crc_seed = 0xFFFFFFFF
 xor_out = 0
-#xor_out = 0xFFFFFFFF
 # CRCs are compatible with Renesas Synergy CRC hardware used the dumb way
 # for CRC32-C: seed=0, xor_out=0, poly=0x11EDC6F41, reverse=false, reverse order of each 4-byte chunk
 # for CRC32: seed=(seed), xor_out=0, poly=0x104C11DB7, reverse=false, reverse order of each 4-byte chunk
 
-#from tqdm import tqdm
 crc_f = crcmod.mkCrcFun(crc_poly, rev=False, initCrc=crc_seed, xorOut=xor_out)
+
+###########################
+# Global helper functions #
+###########################
 
 # Helper function for displaying time prettily
 def formatted_time(t):
@@ -72,6 +71,10 @@ def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, l
     if iteration == total: 
         print()
         
+###########################
+# Communications routines #
+###########################
+
 def crc_4(arr):
     crc = crc_seed
     i = 0
@@ -114,15 +117,12 @@ def read_rsp(port, rsp_len, rsp_id):
     rsp_data = port.read(rsp_len)
     if len(rsp_data) >= rsp_len:
         if msg_is_valid(rsp_data, rsp_id):
-            #print(rsp_data)
             return rsp_data
         else:
             print("! Invalid response {}".format(rsp_data))
             return bytearray()
     else:
         print("! Error: Serial timeout")
-        #print(rsp_data)
-        #print(bytearray())
         return bytearray()
 
 def handle_none(_port, _args):
@@ -134,8 +134,6 @@ def handle_ping(port, args):
     cmd_data = make_cmd(cmd_len, 2)
 
     insert_crc(cmd_data)
-
-    #print(' '.join("{:02X}".format(B) for B in cmd_data))
 
     port.write(cmd_data)
     rsp_data = read_rsp(port, rsp_len, 3)
@@ -163,13 +161,6 @@ def handle_vt_get_bit_count_kpage(port, base_address,read_mv):
         bitarray = np.unpackbits(array)#np.unpackbits(array.view(np.uint8))
 
         NDarray = np.reshape(bitarray, (1024, 8))   #np.reshape(bitarray, (512, 16)) #if we are reading 16 bit words
-
-        # for line in range(63):
-        #     print(line,4+64*line,4+64*line+15)
-        #     print("  {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X}".format(rsp_data[4+64*line + 0], rsp_data[4+64*line + 1], rsp_data[4+64*line + 2], rsp_data[4+64*line + 3],
-        #                                                                                                                                      rsp_data[4+64*line + 4], rsp_data[4+64*line + 5], rsp_data[4+64*line + 6], rsp_data[4+64*line + 7],
-        #                                                                                                                                      rsp_data[4+64*line + 8], rsp_data[4+64*line + 9], rsp_data[4+64*line +10], rsp_data[4+64*line +11],
-        #                                                                                                                                      rsp_data[4+64*line +12], rsp_data[4+64*line +13], rsp_data[4+64*line +14], rsp_data[4+64*line +15]))
 
     else:
         return False
@@ -247,10 +238,8 @@ def handle_get_sector_bit_count(port, sector_address,read_mv):
     rsp_data = read_rsp(port, rsp_len, 17)
     if len(rsp_data) >= rsp_len:
         bits_set, = struct.unpack_from("<I", rsp_data, 4)
-        #print"Sector: {}\nVoltage: {}mV\nCounts: {}\n".format(sector_address, read_mv, bits_set))
     else:
         return False
-    #print(bits_set)
     return bits_set
 
 def handle_read_data(port, address,read_mv,vt,bit_mode, max_repeats=3, current_iter=1):
@@ -362,6 +351,10 @@ def handle_ana_set_active_counts(port, args):
         return False
     return True
 
+#####################
+# Analysis routines #
+#####################
+
 
 def hex_int(x):
     return int(x, 16)
@@ -388,12 +381,12 @@ def read_chip_voltages(port,voltages,sectors,location='.'):
                 else:
                     saved_array = np.vstack((saved_array,NDarray))
                     #print(saved_array)
-                #sg.one_line_progress_meter('Getting Sweep Data', count + 1, len(voltages) * 1024 *128)
                 time.sleep(0.003) # This doesn't end up slowing us down much, but it does avoid costly serial timeout errors.
                 printProgressBar(count, len(voltages)*128*len(sectors), prefix='Getting sweep data: ', suffix='complete', decimals=0, length=50)
                 count+=1
             np.savetxt(os.path.join(location,"data-{}-{}.csv".format(j,base_address)), saved_array, fmt='%d', delimiter='')
 
+	print()
     return
 
 ## No longer used
@@ -408,7 +401,6 @@ def get_voltage_sweep(port,base_address,voltages,method):
                 NDarray = handle_read_data(port, address, j,1,1)
                 if not isinstance(NDarray,bool):
                     temp.append(NDarray)
-                #sg.one_line_progress_meter('Getting Sweep Data', tracker + 1, len(voltages)*128)
                 printProgressBar(tracker+1, len(voltages)*128, prefix='Getting sweep data: ', suffix='complete', decimals=0, length=50)
                 tracker+=1
             values.append([j, sum(temp)])
@@ -424,7 +416,6 @@ def get_voltage_sweep(port,base_address,voltages,method):
             bits.append(bit_count)
             time.sleep(.003)
             printProgressBar(idx, len(voltages), prefix='Getting sweep data: ', suffix='complete', decimals=0, length=50)
-            #sg.one_line_progress_meter('Getting Sweep Data', idx1 + 1, len(voltages))
     if plt.fignum_exists(1):
         ax1 = plt.figure(1).axes[0]
         ax2 = plt.figure(1).axes[1]
@@ -445,6 +436,7 @@ def get_voltage_sweep(port,base_address,voltages,method):
     ax1.legend(loc='best')
     plt.show()
     return
+
 def get_bit_noise(port, sector_address, voltages,iterations):
     max = iterations
     real_count = 0
@@ -489,6 +481,10 @@ def get_bit_noise(port, sector_address, voltages,iterations):
     plt.hist(true_values,bins=n_bins)
     print(np.mean(true_values),np.std(true_values))
     plt.show()
+
+#######
+# CLI #
+#######
 
 # CLI spec
 parser = ArgumentParser(description="CLI for chip reads/writes")
@@ -600,92 +596,3 @@ elif(args.write):
     elif(args.all_sectors):
         handle_program_chip(ser, args.value)
         print("Writing a chip takes about 30 minutes and will continue despite the serial error that causes this macro to abort. Please wait until the chip stops flashing.")
-
-    
-#ports = serial.tools.list_ports.comports()
-#port_names = []
-#for port, desc, hwid in sorted(ports):
-#    port_names.append(port)
-#
-#tab1_layout =  [[sg.Listbox(values=port_names, size=(30, 3), key='-PORTS-', enable_events=True)],
-#                [sg.Text('Version', size=(15, 1)), sg.Text('', size=(15, 1),key='-VERSION-')],
-#                [sg.Text('Idle?', size=(15, 1)), sg.Text('', size=(15, 1),key='-IDLE-')]
-#                ]
-#tab3_layout =  [[sg.Text('Sector Address', size=(15, 1)), sg.InputText('800000',key = '-SECTOR ADDRESS PROGRAM-')],
-#                [sg.Text('Value', size=(15, 1)), sg.InputText('0x5555', size=(15, 1),key='-SECTOR VALUE-')],
-#                [sg.Button('Program Chip', size=(15, 1), key = '-PROGRAM CHIP-'),sg.Button('Erase Chip', size=(15, 1), key = '-ERASE C#HIP-')],
-#                [sg.Button('Program Sector', size=(15, 1), key = '-PROGRAM SECTOR-'),sg.Button('Erase Sector', size=(15, 1), key = '-E#RASE SECTOR-')]
-#                ]
-#tab2_layout = [
-#    [sg.Text('Sector Address', size=(15, 1)), sg.InputText('800000',key = '-SECTOR ADDRESS-')],
-#    [sg.Text('Sweep Start (mv)', size=(15, 1)), sg.InputText('2000',key = '-START MV-')],
-#    [sg.Text('Sweep End (mv)', size=(15, 1)), sg.InputText('4000',key = '-END MV-')],
-#    [sg.Text('Step (mV)', size=(15, 1)), sg.InputText('200',key = '-STEP MV-')],
-#    [sg.Text('Iterations (#)', size=(15, 1)), sg.InputText('5',key = '-ITERS NOISE-')],
-#    [sg.Checkbox('Calculation on Chip (1) vs on PC (0)', default=True,key = '-METHOD-')],
-#    [sg.Button('Sweep Voltages', key = '-SWEEP SUBMIT-'),sg.Button('Bit Noise', key = '-BIT NOISE-'),sg.Button('Sweep Chip', key = '-S#WEEP CHIP SUBMIT-')]
-#]
-#layout = [[sg.TabGroup([[sg.Tab('Board Config', tab1_layout, tooltip='tip'),sg.Tab('Board Program', tab3_layout, tooltip='tip'), sg.Ta#b('Voltage Sweep', tab2_layout)]], tooltip='TIP2')],
-#          [sg.Button('Exit')]]
-#window = sg.Window('Neutron GUI', layout, default_element_size=(15,1))
-#plt.ion()
-#while True:
-#    event, values = window.read()
-#    if event == sg.WIN_CLOSED or event == 'Exit':
-#        try:
-#            ser.close()
-#        except:
-#            pass
-#        break
-#    if event == '-PORTS-':
-#        print(values['-PORTS-'][0])
-#        try:
-#            ser = serial.Serial(values['-PORTS-'][0], 115200, bytesize = serial.EIGHTBITS,stopbits =serial.STOPBITS_ONE, parity  = ser#ial.PARITY_NONE,timeout=10)
-#        except serial.SerialException as e:
-#            print(e)
-#            window['-VERSION-'].update('N/A')
-#            window['-IDLE-'].update('N/A')
-#
-#            continue
-#        resp = handle_ping(ser, args='')
-#
-#        #handle_ana_get_cal_counts(ser, args='')
-#        if not isinstance(resp,bool):
-#            window['-VERSION-'].update(resp[1])
-#            window['-IDLE-'].update(resp[2])
-#        else:
-#            window['-VERSION-'].update('N/A')
-#            window['-IDLE-'].update('N/A')
-#    elif event == '-SWEEP SUBMIT-':
-#        resp_values = [values['-SECTOR ADDRESS-'],values['-START MV-'],values['-END MV-'],values['-STEP MV-'],values['-METHOD-']]
-#        try:
-#            resp_values = [int(x) for x in resp_values]
-#        except e:
-#            print(e)
-#            continue
-#        get_voltage_sweep(ser, resp_values[0],range(resp_values[1],resp_values[2],resp_values[3]),resp_values[4])
-#    elif event == '-ERASE SECTOR-':
-#        handle_erase_sector(ser,int(values['-SECTOR ADDRESS PROGRAM-']))
-#    elif event == '-ERASE CHIP-':
-#        handle_erase_chip(ser)
-#    elif event == '-PROGRAM SECTOR-':
-#        handle_program_sector(ser,int(values['-SECTOR ADDRESS PROGRAM-']), int(values['-SECTOR VALUE-'],16))
-#    elif event == '-BIT NOISE-':
-#        resp_values = [values['-SECTOR ADDRESS-'], values['-START MV-'], values['-END MV-'], values['-STEP MV-'],values['-ITERS NOISE-#']]
-#        try:
-#            resp_values = [int(x) for x in resp_values]
-#        except e:
-#            print(e)
-#            continue
-#        get_bit_noise(ser, resp[0], range(resp_values[1],resp_values[2],resp_values[3]),resp_values[4])
-#    elif event == '-PROGRAM CHIP-':
-#        handle_program_chip(ser, int(values['-SECTOR VALUE-'],16))
-#    elif event == '-SWEEP CHIP SUBMIT-':
-#        resp_values = [values['-START MV-'], values['-END MV-'], values['-STEP MV-']]
-#        try:
-#            resp_values = [int(x) for x in resp_values]
-#        except e:
-#            print(e)
-#            continue
-#        read_chip_voltages(ser, range(resp_values[0], resp_values[1], resp_values[2]))
-#window.close()
