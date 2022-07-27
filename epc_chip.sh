1f2e49241f35d950b1230d5062155ef32e175e64
Aidan Medcalf <aidanmedcalf@gmail.com>
2022-07-27 10:10:45 -0500

    Added timestamps and better messages

--- epc_chip.sh
+++ epc_chip.sh
@@ -11,19 +11,24 @@ mv_start=5000
 mv_stop=7000
 mv_step=100
 
-echo python3 Neutron_CLI.py --p $port -e --all-sectors
+log () {
+	echo "[`date '+%Y-%m-%d %H:%M:%S'`] $1"
+}
+
+log "Erase all sectors"
 python3 Neutron_CLI.py --p $port -e --all-sectors
 
-echo sleep 10m # delay to allow chip to erase
-sleep 10m
+log "Erase delay 10m"
+sleep 10m # delay to allow the chip to erase
 
-echo python3 Neutron_CLI.py --p $port -w -v 0 --all-sectors
+log "Program device"
 python3 Neutron_CLI.py --p $port -w -v 0 --all-sectors
 
-echo sleep 1h # delay to allow chip to write
-sleep 1h
+log "Program delay 1h"
+sleep 1h # delay to allow the device to write and then settle
 
-echo python3 Neutron_CLI.py --p $port -r --bitcount --all-sectors --start $mv_start --stop $mv_stop --step $mv_step -d $savedir
+log "Writing to $savedir"
+log "Bit count all sectors in mV range [$mv_start, $mv_stop) by $ms_step mV"
 python3 Neutron_CLI.py --p $port -r --bitcount --all-sectors --start $mv_start --stop $mv_stop --step $mv_step -d $savedir
 
-echo 'Complete!'
+log "Complete!"
