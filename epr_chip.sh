b3b074a48be1202678235803f78a7b7a09360148
Aidan Medcalf <aidanmedcalf@gmail.com>
2022-10-08 15:18:22 -0500

    Added safer Bash arguments, more logging, and defaults

--- epr_chip.sh
+++ epr_chip.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 # Arguments:
 #   1    Serial port
 #   2    directory to save to
 
-port=$1
-savedir=$2
+port=${1:-}
+if [[ -z "$port" ]]; then
+	echo "usage: $0 port savedir mvstart mvstop mvstep"
+	exit 1
+fi
+savedir=${2:-}
+if [[ -z "$savedir" ]]; then
+	echo "usage: $0 port savedir mvstart mvstop mvstep"
+	exit 1
+fi
 
-mv_start=4000
-mv_stop=7000
-mv_step=100
+mv_start=${3:-4000}
+mv_stop=${4:-7000}
+mv_step=${5:-100}
+
+# TODO: Validate parameters
 
 log () {
 	echo "[`date '+%Y-%m-%d %H:%M:%S'`] $1"
 }
+
+log "Starting E/P/R"
+log "Port:    $port"
+log "Savedir: $savedir"
+log "Start:   $mv_start mV"
+log "Stop:    $mv_stop mV"
+log "Step:    $mv_step mV"
 
 log "Erase all sectors"
 python3 Neutron_CLI.py --p $port -e --all-sectors
