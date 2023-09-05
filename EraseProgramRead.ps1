9defaff17462b2c05e0aa71df1d6b436810b03a5
Aidan <aidanmedcalf@gmail.com>
2023-09-05 18:05:36 -0500

    EraseProgramRead.ps1: Uncommented sleeps

--- EraseProgramRead.ps1
+++ EraseProgramRead.ps1
@@ -12,13 +12,13 @@ Write-Host "Erase all sectors"
 python Neutron_CLI.py -p $port erase-chip
 
 Write-Host "Erase delay 10m"
-# Start-Sleep -s 600
+Start-Sleep -s 600
 
 Write-Host "Program device"
 python Neutron_CLI.py -p $port write-chip -v 0
 
 Write-Host "Program delay 1h"
-# Start-Sleep -s 3600
+Start-Sleep -s 3600
 
 Write-Host "Read all sectors"
 python Neutron_CLI.py -p $port read --address 0 --sectors 1024 --start $start --stop $stop --step $step -d $dir --format=binary
